b00cc171ffa6df063de7fd1e3f5f03555347eed1
Jack Pope <jackpope1@gmail.com>
2015-10-04 16:19:47 +0000

    Finished setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root :to => "bills#index"
+
+  authenticated :user do
+    root :to => 'bills#index', as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root :to => 'devise/sessions#create'
+  end
+
 end
