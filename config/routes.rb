c7b5c11c93d7aea3d8397d0bc2e12780e9eb4356
Jack Pope <jackpope1@gmail.com>
2015-10-05 18:33:47 +0000

    layout and devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
 
   authenticated :user do
     root :to => 'bills#index', as: :authenticated_root
@@ -7,8 +6,11 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     root :to => 'devise/sessions#create'
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
-
+  
+  devise_for :users
+  
   resources :bills, except: [:new, :edit]
   resources :copayers, only: [:create, :update, :destroy]
 
